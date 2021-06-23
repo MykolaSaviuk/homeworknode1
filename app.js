10ed5201d365ccc9027c31c52573310f7ca1f513
MykolaSaviuk <kolya0603123@gmail.com>
2021-06-23 17:17:49 +0300

    create

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const path = require('path')
 const fs = require('fs')
 
 
-
-
 const folder1800 = path.join(__dirname, '1800');
 const folder2000 = path.join(__dirname, '2000');
 const folderboys = path.join(__dirname, 'boys');
@@ -16,23 +14,24 @@ const move = (fromPath, toPath) => {
             fs.rename(path.join(fromPath, file), path.join(toPath, file), () => {
             })
 
-        })
+        });
+
         const sortByGender = (dirPath, boysDir, girlsDir) => {
             fs.readdir(dirPath, (err, files) => {
-                files.forEach(files => {
-                    fs.readFile(path.join(dirPath, files), (err, data) => {
+                files?.forEach(file => {
+                    fs.readFile(path.join(dirPath, file), (err, data) => {
                         const {gender} = JSON.parse(data);
-                        move(dirPath, gender === 'male' ? boysDir : girlsDir, files);
+                        move(dirPath, gender === 'male' ? boysDir : girlsDir, file);
                     })
                 })
-
             })
 
-        };
+    };
 
-        move(foldergirls, foldergirls);
-        move(folderboys, folderboys);
+        sortByGender(folder2000,foldergirls,folderboys);
+        sortByGender(folder1800,folderboys,foldergirls);
 
-        move(folder1800, folder2000);
-        move(folder2000, folder1800);
+         // move(folder1800, folder2000);
+         // move(folder2000, folder1800);
+
 
